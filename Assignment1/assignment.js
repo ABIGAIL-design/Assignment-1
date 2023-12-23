9a76446a7ef286cee37d84c1a8dda4f9b573910b
Abigail <abigail.alabi2015@gmail.com>
2023-12-24 00:33:37 +0100

    this is my second commit

--- Assignment1/assignment.js
+++ Assignment1/assignment.js
@@ -1,25 +1,25 @@
 
 
-if (a > b && a > c && a > d && a > e){
-alert ("the largest is " + a )
-}
-else if (b > a && b > c && b > d && b > e){
-    alert ("the largest is " + b);
-}
-else if (c > a && c > b && c > d && c > e){
-    alert ("the largest is " + c )
-}
+// if (a > b && a > c && a > d && a > e){
+// alert ("the largest is " + a )
+// }
+// else if (b > a && b > c && b > d && b > e){
+//     alert ("the largest is " + b);
+// }
+// else if (c > a && c > b && c > d && c > e){
+//     alert ("the largest is " + c )
+// }
 
-else if (d > a && d > b && d > c && d > e){
-    alert ("the largest is " + d)
-}
-else {
-alert("the largest is " + e)
-}
+// else if (d > a && d > b && d > c && d > e){
+//     alert ("the largest is " + d)
+// }
+// else {
+// alert("the largest is " + e)
+// }
 
 
-for (var x=0;x<=15; x++){
-    if (x ===0){
-    alert ( x + "is even")
-    }
-}
+// for (var x=0;x<=15; x++){
+//     if (x ===0){
+//     alert ( x + "is even")
+//     }
+// }
